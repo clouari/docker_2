c7ca57415332f60d977ef391d2630d25c3502dc2
Da hee <ekdudekgml35@naver.com>
2022-02-24 14:52:00 +0900

    게시판: 제목을 누르면 전체내용이 보이게 작성한 쿼리

---
+++ html/board/view.php
@@ -0,0 +1,32 @@
+<?php
+     include "../check/db.php";
+
+     $id = $_GET["_id"]; 
+
+     $sql = "SELECT _id,name,title,content FROM board WHERE _id = ".$id;
+     
+     $result = $conn->query($sql);
+
+     $row = $result->fetch_row();
+      
+     $conn->close();
+?>
+
+<html>
+    <head>
+        <title>나만의 게시판</title>
+    </head>
+    <body>
+ 
+
+            작성자 : <?php echo $row[1]?> <br />
+
+            제목 : <?php echo $row[2]?> <br />
+
+            내용 : <?php echo nl2br($row[3])?>
+            <br />
+            <a href="list.php">목록으로</a>
+ 
+
+    </body>
+</html>
