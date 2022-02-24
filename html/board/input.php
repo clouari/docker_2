315589ecc09fe3263f841a502c356775149081df
Da hee <ekdudekgml35@naver.com>
2022-02-24 14:53:07 +0900

    게시판 작성되는 html code

---
+++ html/board/input.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title>나만의 게시판</title>
+    </head>
+    <body>
+
+    <form method="post" action="insert.php">
+
+        작성자: <input type="text" name="name"> <br />
+        제목: <input type="text" name="title"> <br />
+        내용: <textarea style="width:400px;height:300px" name="content"></textarea>
+        <input type="submit" value="등록하기">
+
+    </form>
+    </body>
+</html>
