bd5ae7ee2172b48968a8dfa8383782087b2a8c0d
Da hee <ekdudekgml35@naver.com>
2022-02-24 14:50:58 +0900

    db 가 잘 통신되는지 확인하기

---
+++ html/check/db.php
@@ -0,0 +1,13 @@
+<?php
+$mysql_hostname = 'host.docker.internal';
+$mysql_username = 'Jung';
+$mysql_password = 'dahui';
+$mysql_database = 'myDB';
+$mysql_port = '52000';
+$mysql_charset = 'UTF8';
+$conn = new mysqli($mysql_hostname, $mysql_username, $mysql_password, $mysql_database, $mysql_port, $mysql_charset);
+if($conn->connect_errno){
+    echo 'error : '.$conn->connect_error.'';
+}
+
+?>
