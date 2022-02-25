53f1ab9a3219c05e8b1338e3e01aaceef45efbc2
Da hee <ekdudekgml35@naver.com>
2022-02-25 14:13:18 +0900

    db.php 에 session 연결한 후에, 로그인 화면 만들기

---
+++ html/board/login.php
@@ -0,0 +1,15 @@
+<?php
+
+include "../check/db.php";
+
+?>
+
+<center>
+<form method="post" action="session.php">
+
+    <input type="text" name="id" placeholder="id를 입력 해 주세요">
+    <input type="password" name="pw" placeholder="pw를 입력 해 주세요">
+    <input type="submit" value="로그인">
+
+</form>
+</center>
