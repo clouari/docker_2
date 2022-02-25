499d2424cecc4cfbd71ae5e4a8d49bb4b59f650d
Da hee <ekdudekgml35@naver.com>
2022-02-25 14:41:21 +0900

    앞에 선언을 해 줘야 값이 반영이 되기 때문에 앞에 $id =  하고 선언 해 주기

--- html/board/session.php
+++ html/board/session.php
@@ -1,6 +1,18 @@
 <?php
 
-    echo $_POST["id"];
-    echo $_POST["pw"];
+    session_start();
+
+    
+    $id = $_POST['id'];
+    $pw = $_POST['pw'];
+
+    if($id == "jdh" && $pw == "1234")
+    {
+        echo "로그인 성공";
+        $_SESSION["name"] = "홍길동";
+    }
+    else{
+        echo "로그인 실패";
+    }
 
 ?>
