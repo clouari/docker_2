59b567f686706a3c30ea56f7ae4ffbcfeadb2fb7
Da hee <ekdudekgml35@naver.com>
2022-02-25 14:40:38 +0900

    회원제: 게시판 로그인

--- html/board/login.php
+++ html/board/login.php
@@ -4,12 +4,21 @@ include "../check/db.php";
 
 ?>
 
+<html>
+         <head>
+              <title>로그인화면</title>
+         </head>
+    <body>
+
 <center>
 <form method="post" action="session.php">
 
-    <input type="text" name="id" placeholder="id를 입력 해 주세요">
-    <input type="password" name="pw" placeholder="pw를 입력 해 주세요">
+    <input type="text" name='id' placeholder="id를 입력 해 주세요">
+    <input type="password" name='pw' placeholder="pw를 입력 해 주세요">
     <input type="submit" value="로그인">
 
 </form>
 </center>
+
+    </body>
+</html>
